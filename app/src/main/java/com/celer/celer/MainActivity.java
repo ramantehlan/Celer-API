5ddb433dc49278c2116abc32f90550d2e44cf36c
Raman Tehlan <ramantehlan@gmail.com>
2017-09-10 04:27:15 +0530

    Minor changes

--- app/src/main/java/com/celer/celer/MainActivity.java
+++ app/src/main/java/com/celer/celer/MainActivity.java
@@ -1,13 +1,44 @@
 package com.celer.celer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public TextView output;
+
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+    private void printOutput(String newText){
+
+
+        String content = output.getText().toString();
+        output.setText(content + "\n" + newText);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        output = (TextView) findViewById(R.id.output);
+
+        if(isNetworkAvailable()){
+            printOutput(getResources().getText(R.string.connected).toString());
+        }else{
+            printOutput(getResources().getText(R.string.not_connected).toString());
+        }
     }
+
+
+
 }
